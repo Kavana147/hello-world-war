cd77bcaf7fbb7da4828270998dceb66cdfabfae6
Ubuntu <ubuntu@ip-172-31-26-34.us-west-2.compute.internal>
2022-06-29 02:30:49 +0000

    moving folder to remote

---
+++ kavana/arrayrev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "testing git branch"
+echo "enter the file name to be read in reverse"
+read arrayout
+tac $arrayout > revop
+cat revop | awk '{for(i=NF;i>=1;i--) printf "%s ", $i;print ""}'
+
